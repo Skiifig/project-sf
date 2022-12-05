507ec06a484d55cf28f84f6277b0e88991f1dc99
Skiifig <tristan.reig@gmail.com>
2022-12-05 19:13:08 +0100

    Serveur réparé + Développement confirmation

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy() # création extension
@@ -12,26 +12,29 @@ def index():
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String)
-    password = db.Column(db.String)
 
 with app.app_context():
     db.create_all()
-    
-@app.route("/users/create", methods=["GET", "POST"]) #pour créer son compte
+
+@app.route("/inscription", methods=["GET", "POST"]) #pour créer son compte
 def user_create():
     if request.method == "POST":
         user = User(
-            username=request.form["username"],
-            email=request.form["email"],
+            email=request.form["email"]
         )
+        print(user)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for("user_detail", id=user.id))
+        return redirect(url_for('user_detail', id=user.id))
 
-    return render_template("user/create.html") #tristan, faut créer la page de user_create
+    return render_template("registration.html")
+
+@app.route("/detail")
+def user_detail():
+    id = request.args.get('id')
+    user = db.get_or_404(User, id)
+    return render_template("dashboard.html", user=user)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
-    
---
+++ static/js/autocomplete.js
@@ -0,0 +1,2 @@
+var villes = ['Brest', 'Bordeaux', 'Clermont-Ferrand', 'Dijon', 'Paris', 'Montpellier', 'Toulouse', 'Marseille', 'Nantes', 'Nice', 'Nancy']
+var input = document.getElementById('inputCity')
